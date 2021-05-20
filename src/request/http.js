cc64698cddd2fac9870f17f8dfcdace6f58fbf4a
maomincoding <17864296568@163.com>
2021-05-20 14:34:09 +0800

    v1.1.1

--- src/request/http.js
+++ src/request/http.js
@@ -2,11 +2,11 @@
 import axios from 'axios';
 // 环境的切换
 if (process.env.NODE_ENV === 'development') {
-  axios.defaults.baseURL = 'http://localhost:7788/'; // 开发环境
+	axios.defaults.baseURL = 'http://127.0.0.1:7788/'; // 开发环境
 } else if (process.env.NODE_ENV === 'debug') {
-  axios.defaults.baseURL = ''; // 调试环境
+	axios.defaults.baseURL = ''; // 调试环境
 } else if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = '/'; // 生产环境
+	axios.defaults.baseURL = '/'; // 生产环境
 }
 axios.defaults.timeout = 3000;
 
@@ -16,28 +16,28 @@ axios.defaults.timeout = 3000;
  * @param {Object} params [请求时携带的参数]
  */
 export function get(
-  url,
-  params,
-  config = {
-    add: '',
-  },
+	url,
+	params,
+	config = {
+		add: '',
+	}
 ) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(
-        url,
-        {
-          params,
-        },
-        config,
-      )
-      .then((res) => {
-        resolve(res.data);
-      })
-      .catch((err) => {
-        reject(err.data);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		axios
+			.get(
+				url,
+				{
+					params,
+				},
+				config
+			)
+			.then((res) => {
+				resolve(res.data);
+			})
+			.catch((err) => {
+				reject(err.data);
+			});
+	});
 }
 /**
  * post方法，对应post请求
@@ -45,20 +45,20 @@ export function get(
  * @param {Object} params [请求时携带的参数]
  */
 export function post(
-  url,
-  params,
-  config = {
-    isUpload: false,
-  },
+	url,
+	params,
+	config = {
+		isUpload: false,
+	}
 ) {
-  return new Promise((resolve, reject) => {
-    axios
-      .post(url, params, config)
-      .then((res) => {
-        resolve(res.data);
-      })
-      .catch((err) => {
-        reject(err.data);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		axios
+			.post(url, params, config)
+			.then((res) => {
+				resolve(res.data);
+			})
+			.catch((err) => {
+				reject(err.data);
+			});
+	});
 }
